01be0fa1681a62fccbfb18390ec029b20c09a99f
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-25 22:55:15 +0700

    first commit

---
+++ R/pdb_adhk_triwulanan.R
@@ -0,0 +1,11 @@
+#' Dataset PDB ADHK Triwulanan (2010Q1 - 2025Q2)
+#'
+#' Data ini berisi nilai Produk Domestik Bruto (ADHK) Triwulanan
+#' dalam miliar rupiah dari Triwulan I 2010 hingga Triwulan II 2025.
+#'
+#' @format Objek time series dengan 62 observasi dan frekuensi triwulanan (4).
+#' @source Badan Pusat Statistik (BPS)
+#' @examples
+#' data("pdb_adhk_triwulanan")
+#' plot(pdb_adhk_triwulanan)
+"pdb_adhk_triwulanan"
