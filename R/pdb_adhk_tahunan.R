26fac8692f9115b4d07fbb4bb958df7fa1cf81e7
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-25 23:09:56 +0700

    dataset2

---
+++ R/pdb_adhk_tahunan.R
@@ -0,0 +1,12 @@
+#' Produk Domestik Bruto (ADHK) Tahunan Indonesia, 2010–2024
+#'
+#' Dataset ini berisi nilai Produk Domestik Bruto (PDB) atas dasar harga konstan (ADHK)
+#' dari tahun 2010 hingga 2024 (dalam miliar rupiah) dalam bentuk deret waktu tahunan.
+#'
+#' @format Objek time series (\code{ts}) dengan frekuensi tahunan (2010–2024).
+#' @source Badan Pusat Statistik (BPS)
+#' @examples
+#' data(pdb_adhk_tahunan)
+#' plot(pdb_adhk_tahunan, main = "PDB ADHK Tahunan Indonesia (2010–2024)",
+#'      ylab = "Miliar Rupiah", xlab = "Tahun")
+"pdb_adhk_tahunan"
