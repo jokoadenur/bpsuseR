ab8bca4af7518cb0d2c47d27fe18448e5309d47d
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-26 09:47:20 +0700

    3rd commit

---
+++ R/tingkat_pengangguran_terbuka.R
@@ -0,0 +1,12 @@
+#' Dataset Tingkat Pengangguran Terbuka (2005-2025)
+#'
+#' Data ini berisi Tingkat Pengangguran Terbuka rilis 2 kali per tahunnya
+#' dalam persen
+#'
+#' @format Objek data frame dengan 42 observasi dan 3 variabel:
+#' @source Badan Pusat Statistik (BPS)
+#' @examples
+#' data("tingkat_pengangguran_terbuka")
+#' plot(tingkat_pengangguran_terbuka$Tahun, tingkat_pengangguran_terbuka$TPT, main = "Tingkat Pengangguran Terbuka Indonesia (Februari dan Agustus) 2005–2025 (persen)",
+#'      ylab = "persen", xlab = "Tahun")
+"tingkat_pengangguran_terbuka"
