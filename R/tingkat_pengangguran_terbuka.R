59964148f141d08768ef29fdd3264d325280ab6f
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-26 11:48:24 +0700

    5rd commit

--- R/tingkat_pengangguran_terbuka.R
+++ R/tingkat_pengangguran_terbuka.R
@@ -1,24 +1,19 @@
 #' Dataset Tingkat Pengangguran Terbuka (2005–2025)
 #'
-#' Data ini berisi Tingkat Pengangguran Terbuka rilis 2 kali per tahunnya
-#' dalam persen.
+#' Data ini berisi Tingkat Pengangguran Terbuka (TPT) Indonesia yang dirilis dua kali per tahun (Februari dan Agustus)
+#' oleh Badan Pusat Statistik (BPS), dalam satuan persen (%).
 #'
-#' @format Objek data frame dengan 42 observasi dan 3 variabel:
-#' \describe{
-#'   \item{Tahun}{Tahun pengamatan}
-#'   \item{Bulan}{Bulan pengamatan (Februari atau Agustus)}
-#'   \item{TPT}{Tingkat Pengangguran Terbuka dalam persen}
-#' }
+#' @format Objek time series dengan 41 observasi dan frekuensi dua kali per tahun (`frequency = 2`).
+#' Nilai mulai dari Februari 2005 hingga Februari 2025.
+#'
 #' @source Badan Pusat Statistik (BPS)
+#'
 #' @examples
 #' data("tingkat_pengangguran_terbuka")
-#' # Plot menggunakan kolom spesifik untuk menghindari error plot.data.frame()
 #' plot(
-#'   tingkat_pengangguran_terbuka$Tahun,
-#'   tingkat_pengangguran_terbuka$TPT,
-#'   type = "b",
-#'   main = "Tingkat Pengangguran Terbuka Indonesia (Februari & Agustus) 2005–2025",
-#'   xlab = "Tahun",
-#'   ylab = "Persen"
+#'   tingkat_pengangguran_terbuka,
+#'   main = "Tingkat Pengangguran Terbuka Indonesia (2005–2025)",
+#'   ylab = "Persen (%)",
+#'   xlab = "Tahun"
 #' )
 "tingkat_pengangguran_terbuka"
