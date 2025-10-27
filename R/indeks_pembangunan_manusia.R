dddf3be860fb920ec07296444c30c924b665d466
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-27 16:17:24 +0700

    ipm dataset commit

---
+++ R/indeks_pembangunan_manusia.R
@@ -0,0 +1,17 @@
+#' Dataset Indeks Pembangunan Manusia (2010–2024) dengan Metode Terbaru
+#'
+#' Data ini berisi Indeks Pembangunan Manusia (IPM) Indonesia yang dirilis tahunan
+#' oleh Badan Pusat Statistik (BPS), dalam satuan persen (%). IPM ini menggunakan metode baru dan telah disesuaikan dengan angka Umur Harapan Hidup Hasil Long Form SP2020
+#'
+#' @format Objek time series dengan 15 observasi dan frekuensi tahunan (`frequency = 1`).
+#' Nilai mulai dari Tahun 2010 hingga Tahun 2024.
+#'
+#' @source Badan Pusat Statistik (BPS)
+#'
+#' @examples
+#' indeks_pembangunan_manusia <- indeks_pembangunan_manusia
+#' plot(
+#'   indeks_pembangunan_manusia, main = "Indeks Pembangunan Manusia Indonesia (2010–2024)",
+#'   ylab = "Persen (%)", xlab = "Tahun"
+#' )
+"tingkat_pengangguran_terbuka"
