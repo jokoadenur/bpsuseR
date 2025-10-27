aa3e28e213a6d58aff942a5050f359f65902368b
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2025-10-28 00:24:18 +0700

    regenerate ikg

--- R/indeks_ketimpangan_gender.R
+++ R/indeks_ketimpangan_gender.R
@@ -1,7 +1,7 @@
 #' Dataset Indeks Ketimpangan Gender (2018-2024)
 #'
 #' Data ini berisi Indeks Ketimpangan Gender (IKG) Indonesia yang dirilis tahunan
-#' oleh Badan Pusat Statistik (BPS), dalam satuan (indeks). IKG adalah pengganti ukuran Indeks Pemberdayaan Gender
+#' oleh Badan Pusat Statistik (BPS). IKG adalah pengganti ukuran Indeks Pemberdayaan Gender
 #'
 #' @format Objek time series dengan 7 observasi dan frekuensi tahunan (`frequency = 1`).
 #' Nilai mulai dari Tahun 2018 hingga Tahun 2024.
