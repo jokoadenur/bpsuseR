b6803e76372e8ced2d57490e4d6f5f207aaa16e9
Joko Ade Nursiyono <jokoadenursiyono@gmail.com>
2026-01-08 12:48:53 +0700

    Update DESCRIPTION and add PDB ADHK Triwulanan dataset

--- R/pdb_adhk_triwulanan.R
+++ R/pdb_adhk_triwulanan.R
@@ -7,6 +7,6 @@
 #' @source Badan Pusat Statistik (BPS)
 #' @examples
 #' pdb_adhk_triwulanan <- pdb_adhk_triwulanan
-#' plot(pdb_adhk_triwulanan, main = "PDB ADHK Triwulanan Indonesia (2010-2025)",
-#'      ylab = "Miliar Rupiah", xlab = "Tahun")
+#' plot(pdb_adhk_triwulanan$Qtr1, main = "PDB ADHK Triwulanan Indonesia (2010-2025)"
+#'      )
 "pdb_adhk_triwulanan"
